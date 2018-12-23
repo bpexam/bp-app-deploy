b6d6a29c056ff69d200e14247fcac647406d961e
bpexam <45976716+bpexam@users.noreply.github.com>
2018-12-23 23:06:00 +0200

    update main.py
    adding main function, edge cases, cleanup for containers and logging

--- main.py
+++ main.py
@@ -1,51 +1,77 @@
-import requests
+import sys
 import os
+import requests
 import time
+import tarfile
+import logging
 
 #### Configuration
 s3FilePath = 'https://s3.eu-central-1.amazonaws.com/devops-exercise/pandapics.tar.gz'
 imageFolder = './public/images'
 healthCheckUrl = 'http://localhost:3000/health'
 warmUptime = 15 #This value might need tweak on slow connection to retrive the images
+logging.basicConfig(filename='deploy.log', level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(name)s %(message)s')
+logger=logging.getLogger(__name__)
 
 def getImages(s3FilePath,imageFolder):
-    print('Beginning image download from S3')
-    r = requests.get(s3FilePath)
-    with open('pandapics.tar.gz', 'wb') as f:
-        f.write(r.content)
-    if r.status_code == 200:
-        print "Succesfully downloaded images"
-        print "Checking if images directory exists and creating it if not"
-        if not os.path.exists(imageFolder):
-            os.makedirs(imageFolder)
-            print "Image folder created"
+    print('Starting image download from S3')
+    try:
+        r = requests.get(s3FilePath)
+        with open('pandapics.tar.gz', 'wb') as f:
+            f.write(r.content)
+        if r.status_code == 200:
+            print "Succesfully downloaded images"
+            print "Extracting images into image folder"
+            tar = tarfile.open("pandapics.tar.gz")
+            tar.extractall(path=imageFolder)
+            tar.close()
+            print "Clean up archive"
+            os.remove("pandapics.tar.gz")
         else:
-            print "Image folder already exists"
-        print "Untar our downloaded images to the image folder"
-        os.system("tar -xf pandapics.tar.gz -C " + imageFolder)
-        print "Cleanup the archive file"
-        os.system("rm -f pandapics.tar.gz")
-    else:
-        print "There was an error downloading images"
+            msg="There was an error downloading images error" + r.status_code
+            print msg
+            logger.error(msg)
+    except requests.exceptions.RequestException as e:
+        print e
+        logger.error(e)
+        sys.exit(1)
+
+def startContainers():
+    try:
+        os.system('docker-compose up -d')
+    except OSError as e:
+        print e
+        logger.error(e)
+        sys.exit(1)
 
 def healthCheck(healthCheckUrl):
     print "Checking app health status"
-    r = requests.get(healthCheckUrl)
-    if r.status_code == 200:
-        print "App & DB deployed Succesfully"
-    elif r.status_code == 500:
-        print "Issue with DB or Disk connectivity"
-    else:
-        print "Unknown error has occured"
+    try:
+        r = requests.get(healthCheckUrl)
+        if r.status_code == 200:
+            print "App & DB deployed Succesfully"
+        else:
+            msg="App failed to start"+r.status_code
+            print msg
+            logger.error(msg)
+    except requests.exceptions.RequestException as e:
+        msg="App failed the healthcheck "+str(e)+"\n Removing containers"
+        os.system('docker-compose down -d')
+        print msg
+        logger.error(msg)
+        sys.exit(1)
 
+def main():
+    print "Starting deployment procedure"
+    logger.info("-------- \n Starting deployment procedure\n--------")
+    getImages(s3FilePath,imageFolder)
+    print "Starting Docker containers"
+    startContainers()
+    print "Waiting for container warmup ( "+str(warmUptime)+" seconds)"
+    time.sleep(warmUptime)
+    print "Checking container health"
+    healthCheck(healthCheckUrl)
 
-
-### deployment procedure
-print "Starting deployment procedure"
-getImages(s3FilePath,imageFolder)
-print "Starting Docker containers"
-os.system('docker-compose up -d')
-print "Waiting for container warmup ( " + str(warmUptime) + "seconds)"
-time.sleep(warmUptime)
-print "Checking container health"
-healthCheck(healthCheckUrl)
+if __name__ == '__main__':
+    main()
